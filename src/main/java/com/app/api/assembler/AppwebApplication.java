aa70f6200fc9c10757eea86780be027c3e34b67f
vitorguilhen <vitorribeiroguilhen@gmail.com>
2022-06-21 12:54:38 -0300

    packages criados

---
+++ src/main/java/com/app/api/assembler/AppwebApplication.java
@@ -0,0 +1,13 @@
+package com.app.api.assembler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppwebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/domain/teste.java
@@ -0,0 +1,5 @@
+package com.app.domain;
+
+public class teste {
+
+}
